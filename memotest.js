66d93abdec01f467fb3488aa3fbc142d1f8755ee
Federico Aldrighetti <f.a.aldrighetti@gmail.com>
2023-03-11 16:03:35 -0300

    Mezcla los colores en cada ficha
    El programa consta de 16 fichas, dos de cada color, todas ordenadas aleatoriamente.

--- memotest.js
+++ memotest.js
@@ -2,10 +2,20 @@
 //Ubicarlas aleatoriamente antes de empezar la partida
 //Asignarles clase tapado y que con el clic se remueva esa clase
 //Si el id de las imágenes cliqueadas coincide, se remueve la clase tapada para esa imagen. Sino, se ocultan.
-//Que al final del juego salga un alert que diga "Volver a jugar" y recargue la página
+//Que al final del juego salga un botón que diga "Volver a jugar" y recargue la página
 
-/* 
+let arrayColores = ['rojo','azul','verde','amarillo','violeta','naranja','rosa','celeste'];
+arrayColores = arrayColores.concat(arrayColores);
+const tablero = document.querySelectorAll('.ficha');
 
+tablero.forEach(element => {
+    let sorteado = Math.floor(Math.random() * arrayColores.length);
+    element.classList.add(arrayColores[sorteado]);
+    arrayColores.splice(sorteado,1);
+    console.log(arrayColores);
+});
+
+/*
 function mezclarFichas(){
     const arrayImagenes = [imagen1, imagen2, ...]
     const arrayJuego = arrayImagenes.concat(arrayImagenes)
@@ -14,7 +24,6 @@ function mezclarFichas(){
 }
 
 function manejarTurno(){
-
 }
 
 function manejarClic(){
@@ -33,5 +42,3 @@ function manejarClic(){
 mezclarFichas()
 manejarTurno()
 */
-let array = [4,5,6]
-console.log(array.concat(array))
