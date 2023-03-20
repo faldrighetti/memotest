bbb3956d81be019ed689dc425a6ca7b16b7c7cb2
Federico Aldrighetti <f.a.aldrighetti@gmail.com>
2023-03-20 19:35:30 -0300

    Termina programa
    Agrega pantalla final con cantidad de rondas

--- memotest.js
+++ memotest.js
@@ -1,13 +1,8 @@
-//SI Poner x imágenes en un array y duplicarlas.
-//SI Ubicarlas aleatoriamente antes de empezar la partida
-//Asignarles clase tapado y que con el clic se remueva esa clase
-//Si las fichas cliqueadas coinciden, se remueve la clase ficha para ellas, pero se mantiene el color. Sino, se ocultan.
-//Que al final del juego salga un botón que diga "Volver a jugar" y recargue la página
-
 let rondas = 0;
 let arrayColores = ['rojo','azul','verde','amarillo','violeta','naranja','rosa','celeste'];
 arrayColores = arrayColores.concat(arrayColores);
 const tablero = document.querySelectorAll('.ficha');
+let tableroNuevo = Array.from(tablero);
 let clics = 0;
 let botonesOprimidos = [];
 
@@ -18,17 +13,11 @@ function manejarClic (evento){
     botonesOprimidos.push(botonOprimido);
 
     if (clics === 2) {
-        console.log(botonesOprimidos);
         if(botonesOprimidos[0].id == botonesOprimidos[1].id && botonesOprimidos[0].name !== botonesOprimidos[1].name){
-            console.log("Encontraste una pareja!!");
             destaparPareja(botonesOprimidos[0], botonesOprimidos[1]);
-            let tableroNuevo = Array.from(tablero);
             tableroNuevo.splice(botonesOprimidos[0],1);
             tableroNuevo.splice(botonesOprimidos[1],1);
-            console.log(tableroNuevo.length + " length");
             rondas++;
-            ganarPartida();
-                    //Tengo que destapar definitivamente el color encontrado antes de llamar a esta función.
         } else if(botonesOprimidos[0].name == botonesOprimidos[1].name){
             ocultarTodosLosColores();
         } else{
@@ -40,7 +29,7 @@ function manejarClic (evento){
         clics = 0;
         botonesOprimidos = [];
     }
-    console.log(clics);
+    ganarPartida();
 }
 
 function destaparPareja(cuadro1, cuadro2){
@@ -58,11 +47,9 @@ tablero.forEach(element => {
     let sorteado = Math.floor(Math.random() * arrayColores.length);
     let color = arrayColores[sorteado];
     element.classList.add(color);
-    //Con esto el elemento adquiere la clase de su color
     element.id = color;
     //Con esto el elemento adquiere el id de su color. Cuando se borre la clase,
     //seguirá teniendo el id que lo vincule con el color
-
     ocultarColor(element, color);
     arrayColores.splice(sorteado,1);
 });
@@ -81,11 +68,6 @@ function descubrirFicha(elemento){
     elemento.classList.remove('tapado');
 }
 
-//Cuando se encuentre una pareja de fichas, sacarles la clase ficha para que no se oculten luego,
-//y que solo tengan la de su color
-
-//Idea: const cliqueados = document.querySelectorAll ('.cliqueados'). If cliqueados[0].id === cliqueados[1].id = ganamos.
-
 function ocultarTodosLosColores(){
     tablero.forEach(element => {
         if(!element.classList.contains('descubierto')){
@@ -95,30 +77,22 @@ function ocultarTodosLosColores(){
     });
 }
 
-/*
-function manejarTurno(){
-}
-
-function manejarClic(){
-    boton.onclick = remover clase tapado
-    if(document.querySelectorAll('.descubierto').length === 2){
-        if (boton.id === boton2.id) {
-            boton.classList.remove('tapado')
-            boton2.classList.remove('tapado')
-        } else {
-            boton.classList.add('tapado')
-            boton2.classList.add('tapado')
-        }
-    }
-}
-
-mezclarFichas()
-manejarTurno()
-*/
-
 function ganarPartida(){
-    let descubiertos = document.querySelectorAll('.descubierto');
-    if(!tablero.length){
-        console.log("Ganaste en " + rondas + " rondas");
-    }
+    if(!tableroNuevo.length){
+        ocultarTablero();
+        escribirVictoria();
+    }   
+}
+
+function ocultarTablero(){
+    const $tablero = document.querySelector('#tablero');
+    $tablero.className = 'oculto';
+}
+
+function escribirVictoria(){
+    const mensaje = document.createElement('p');
+    const $resultado = document.querySelector('#resultado');
+    $resultado.className = '';
+    mensaje.textContent = "¡Ganaste la partida en " + rondas + " rondas!"
+    $resultado.appendChild(mensaje);
 }
