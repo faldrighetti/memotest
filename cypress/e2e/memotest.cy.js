8fb56c2e2268ccbcafb4d4ab4b7b2cbefda72e06
Federico Aldrighetti <f.a.aldrighetti@gmail.com>
2023-03-22 18:45:54 -0300

    Agrega intento exitoso de prueba
    Ingresa a Google en spec.cy.js

--- cypress/e2e/memotest.cy.js
+++ cypress/e2e/memotest.cy.js
@@ -1,3 +1,9 @@
+/*describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})*/
+
 //No puedo entrar a una URL desde el servidor
 //Ver si puedo abrir un servidor para darle una URL donde testear o si puedo hacerlo desde el archivo en la pc local
 //Sino, puedo intentarlo en el deploy de GitHub Pages.
