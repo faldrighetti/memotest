6f19a0e66d16cd66573fd3cab74cfb96b683436f
Federico Aldrighetti <f.a.aldrighetti@gmail.com>
2023-03-22 17:40:02 -0300

    Agrega archivos y dependencia Cypress

---
+++ cypress/e2e/memotest.js
@@ -0,0 +1,3 @@
+//No puedo entrar a una URL desde el servidor
+//Ver si puedo abrir un servidor para darle una URL donde testear o si puedo hacerlo desde el archivo en la pc local
+//Sino, puedo intentarlo en el deploy de GitHub Pages.
