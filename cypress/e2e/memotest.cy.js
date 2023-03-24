9d9e9c2b9fa00c3a3ae10d1e58df2552cd95c58d
Federico Aldrighetti <f.a.aldrighetti@gmail.com>
2023-03-24 03:28:38 -0300

    Agrega primeros tests con cypress
    Hay un bug en el test de asegurar que los cuadros sean aleatorios, no detecta el cy.get para ningún elemento

--- cypress/e2e/memotest.cy.js
+++ cypress/e2e/memotest.cy.js
@@ -1,20 +1,71 @@
-/*describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})*/
-
-//No puedo entrar a una URL desde el servidor
-//Ver si puedo abrir un servidor para darle una URL donde testear o si puedo hacerlo desde el archivo en la pc local
-//Sino, puedo intentarlo en el deploy de GitHub Pages.
-
-const URL = 'http://www.google.com'; //Acá va la URL
+const URL = 'http://127.0.0.1:8080/memotest.html';
+const URL2 = 'http://192.168.0.23:8080/memotest.html';
+const NUMERO_CUADROS = 16;
 
 context('memotest', () => {
     before(() => {
         cy.visit(URL);
-    })
+    });
 
-    it('tests', () => {})
-    
-})
+    //Test 1
+    it('Se asegura de que haya un tablero con cuadros', () => {     
+      cy.get('#tablero').find('.ficha').should('have.length', NUMERO_CUADROS);
+    });
+
+    //Test 2
+    /*it('Se asegura de que los cuadros sean aleatorios', () => {
+      let fichasOriginales = [];
+      let fichasNuevas = [];
+      cy.get('.ficha').then((cuadros) => {
+        
+        cuadros.each(function(i, cuadro) {
+          fichasOriginales.push(cuadro.id);
+        });
+
+        cy.visit(URL);
+
+        cy.get('.ficha').then((cuadros) => {
+          cuadros.each(function(i, cuadro) {
+            fichasNuevas.push(cuadro.id);
+          });
+
+          cy.wrap(fichasOriginales).should('not.deep.equal', fichasNuevas);
+
+        });
+      });
+    });*/
+
+    //Test 3
+    /*it('Se asegura de que se vuelvan a tapar las fichas después de un clic erróneo', () => {
+
+    });*/
+
+
+    //Test 4
+    /*it('Se asegura de que se vean las parejas descubiertas', () => {
+
+    });*/
+
+    it('se asegura que los cuadros sean aleatorios', () => {
+      cy.get('button').then((cuadros) => {
+        let clasesOriginales = [];
+        cuadros.each(function(i, cuadro) {
+          clasesOriginales.push(cuadro.id);
+        });
+
+        cy.visit(URL2);
+
+        let clasesNuevas = [];
+        cy.get('button').then(nuevosCuadros => {
+          nuevosCuadros.each(function(i, cuadro) {
+            clasesNuevas.push(cuadro.id);
+          });
+
+          cy.wrap(clasesOriginales).should('not.deep.equal', clasesNuevas);
+        });
+      });
+      
+
+    });
+  
+});
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,16 @@
-describe('template spec', () => {
+const URL = 'http://127.0.0.1:8080/memotest.html';
+
+/*describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://www.google.com');
+    cy.visit(URL);
   })
-})
+})*/
+
+context('Memotest', () => {
+  before (() => {
+    cy.visit('https://www.google.com');
+  });
+
+  //describe('juega al memotest')
+  it('tests', () => {});
+});
